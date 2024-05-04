2cb454596f76ed332e3012818577c57df77db3d5
Tim Irvin <irvin@nettempo.com>
2024-05-03 17:35:40 -0700

    Updated for AWS provider 5

--- kinesis_firehose.tf
+++ kinesis_firehose.tf
@@ -4,8 +4,8 @@ resource "aws_kinesis_firehose_delivery_stream" "cloudwatch" {
 
   extended_s3_configuration {
     bucket_arn      = aws_s3_bucket.cloudwatch_firehose.arn
-    buffer_interval = 60
-    buffer_size     = 5
+    buffering_interval = 60
+    buffering_size     = 5
     prefix          = var.s3_bucket_prefix
     role_arn        = aws_iam_role.cloudwatch_firehose.arn
     error_output_prefix = "error/"
